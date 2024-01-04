3297fb54f323e8e1fa6bb693196023d8281bc446
LukasSeratowicz <127187274+LukasSeratowicz@users.noreply.github.com>
2024-01-04 13:28:35 +0000

    fix a typo

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -349,7 +349,7 @@
                                     on:keypress={(e) => {if(e.key === 'Enter') clicked(x, y)}} 
                                     on:contextmenu|preventDefault={() => rightClicked(x, y)}
                                     class="sapper_tile" 
-                                    style="background-image: url('/images/skins/${current_skin}/{
+                                    style="background-image: url('/images/skins/{current_skin}/{
                                         tileData[(max_x*y + x).toString()]
                                     }.svg')"
                                 >
