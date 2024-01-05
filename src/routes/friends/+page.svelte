9e632be14cb039fb2976c4cc8fad1007addca8b9
LukasSeratowicz <127187274+LukasSeratowicz@users.noreply.github.com>
2024-01-05 14:13:57 +0000

    Added avatar skins

--- src/routes/friends/+page.svelte
+++ src/routes/friends/+page.svelte
@@ -3,16 +3,17 @@
     import Friend from './Friend.svelte';
 
     let friends_list = [
-          { id: 1, name: 'Lukas', image: '/images/avatars/avatar1.png' },
-          { id: 2, name: 'Anna', image: '/images/avatars/avatar2.png' },
-          { id: 3, name: 'John', image: '/images/avatars/avatar2.png' },
-          { id: 4, name: 'Emily', image: '/images/avatars/avatar2.png' },
-          { id: 5, name: 'Michael', image: '/images/avatars/avatar2.png' },
-          { id: 6, name: 'Sophia', image: '/images/avatars/avatar2.png' },
-          { id: 7, name: 'Daniel', image: '/images/avatars/avatar2.png' },
-          { id: 8, name: 'Olivia', image: '/images/avatars/avatar2.png' },
-          { id: 9, name: 'David', image: '/images/avatars/avatar2.png' },
-          { id: 10, name: 'Emma', image: '/images/avatars/avatar2.png' },
+          { id: 1, name: 'Lukas', image: '/images/avatars/1.png' },
+          { id: 2, name: 'Anna', image: '/images/avatars/2.png' },
+          { id: 3, name: 'John', image: '/images/avatars/3.png' },
+          { id: 4, name: 'Emily', image: '/images/avatars/4.png' },
+          { id: 5, name: 'Michael', image: '/images/avatars/5.png' },
+          { id: 6, name: 'Sophia', image: '/images/avatars/6.png' },
+          { id: 7, name: 'Daniel', image: '/images/avatars/7.png' },
+          { id: 8, name: 'Olivia', image: '/images/avatars/8.png' },
+          { id: 9, name: 'David', image: '/images/avatars/9.png' },
+          { id: 10, name: 'Emma', image: '/images/avatars/10.png' },
+          { id: 11, name: 'Evelina', image: '/images/avatars/11.png' },
     ];
 </script>
 
