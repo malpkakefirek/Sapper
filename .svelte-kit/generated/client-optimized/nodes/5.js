7869fe19292b2812da337e066a054e92a251f4fb
malpkakefirek <72975781+malpkakefirek@users.noreply.github.com>
2024-01-05 13:26:21 +0000

    removed junk

--- .svelte-kit/generated/client-optimized/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/game/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/game/+page.svelte";
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.680f1a26.js","_app/immutable/chunks/scheduler.3b48bc77.js","_app/immutable/chunks/index.e543c91d.js","_app/immutable/chunks/stores.b51042c6.js","_app/immutable/chunks/singletons.4a9ba403.js","_app/immutable/chunks/index.9991ffd9.js"];
-export const stylesheets = [];
-export const fonts = [];
