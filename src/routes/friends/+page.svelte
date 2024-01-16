d715ba05b7b0d5fd294f79556f8b9b5393ff8e48
malpkakefirek <72975781+malpkakefirek@users.noreply.github.com>
2024-01-16 19:33:11 +0000

    float precision in friends

--- src/routes/friends/+page.svelte
+++ src/routes/friends/+page.svelte
@@ -51,11 +51,11 @@
     });
 
     function display_time(time){
-        let seconds = parseFloat(time)/100;
+        let seconds = (parseFloat(time)/100).toFixed(2);
 
         let hours = Math.floor(seconds / 3600);
         let minutes = Math.floor((seconds % 3600) / 60);
-        let remainingSeconds = (seconds % 60);
+        let remainingSeconds = (seconds % 60).toFixed(2);
 
         let display = '';
 
