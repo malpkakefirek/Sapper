33b676c6d2fc183621c03d458f30ad25a5b74f35
malpkakefirek <72975781+malpkakefirek@users.noreply.github.com>
2024-01-17 20:36:29 +0000

    change register page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -57,72 +57,77 @@
     <meta name="description" content="Register to the app" />
 </svelte:head>
 
-<form
-    method="post" 
-    on:submit|preventDefault={handleSubmit}
-    use:form2
-    action="https://sapper-api.onrender.com/register" 
->
-    <h1>Register</h1>
-    <div style="display: flex; justify-content: center;">
-        <input type="email" name="email" use:validators={[required, email]} />
-        <HintGroup for="email">
-            <Hint on="required">This is a mandatory field</Hint>
-            <Hint on="email" hideWhenRequired>Email is not valid</Hint>
-        </HintGroup>
-        
-        <input 
-            type="username" 
-            name="username" 
-            use:validators={[
-                required, 
-                pattern("^[a-zA-Z0-9_]+$"), 
-                minLength(5), 
-                maxLength(24)
-            ]} 
-        />
-        <HintGroup for="username">
-            <Hint on="required">This is a mandatory field</Hint>
-            <Hint on="pattern" hideWhenRequired>Username can only contain letters, numbers and underscores</Hint>
-            <Hint on="minLength" hideWhenRequired>Username must be at least 5 characters long</Hint>
-            <Hint on="maxLength" hideWhenRequired>Username must be at most 24 characters long</Hint>
-        </HintGroup>
-
-        <input 
-            type="password" 
-            name="password" 
-            use:validators={[
-                required,
-                minLength(8),
-                maxLength(64),
-                passwordMatch
-            ]} 
-        />
-        <HintGroup for="password">
-            <Hint for="password" on="required">This is a mandatory field</Hint>
-            <Hint for="password" on="minLength" hideWhenRequired>Password must be at least 8 characters long</Hint>
-            <Hint for="password" on="maxLength" hideWhenRequired>Password must be at most 64 characters long</Hint>
-            <Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>
-        </HintGroup>
-        
-        <label for="passwordConfirmation">Password Confirmation</label>
-        <input
-          type="password"
-          name="passwordConfirmation"
-          use:validators={[required, passwordMatch]}
-        />
-        <HintGroup for="passwordConfirmation">
-          <Hint on="required">This is a mandatory field</Hint>
-          <Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>
-        </HintGroup><br />
-
-        <button disabled={!$form2.valid}>Login</button>
-        <!-- <button>Login</button> -->
-    </div>
-</form>
-<pre>
-
-</pre>
+<div class="form dropbox">
+    <form
+        method="post" 
+        on:submit|preventDefault={handleSubmit}
+        use:form2
+        action="https://sapper-api.onrender.com/register" 
+    >
+        <div class="title">Register</div>
+        <div class="subtitle">please input current and new password below</div>
+        <div style="display: flex; align-content: center; flex-direction: column;">
+            <div class="input-container ic1">
+                <input type="email" class="input" name="email" use:validators={[required, email]} />
+                <HintGroup for="email">
+                    <Hint on="required">This is a mandatory field</Hint>
+                    <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+                </HintGroup>
+                <div class="cut"></div>
+                <label for="email" class="placeholder">Email</label>
+            </div>
+            <div class="input-container ic2">
+                <input 
+                    type="username" 
+                    class="input"
+                    name="username" 
+                    use:validators={[required, pattern("^[a-zA-Z0-9_]+$"), minLength(5), maxLength(24)]} 
+                />
+                <HintGroup for="username">
+                    <Hint on="required">This is a mandatory field</Hint>
+                    <Hint on="pattern" hideWhenRequired>Username can only contain letters, numbers and underscores</Hint>
+                    <Hint on="minLength" hideWhenRequired>Username must be at least 5 characters long</Hint>
+                    <Hint on="maxLength" hideWhenRequired>Username must be at most 24 characters long</Hint>
+                </HintGroup>
+                <div class="cut"></div>
+                <label for="username" class="placeholder">Username</label>
+            </div>
+            <div class="input-container ic2">
+                <input 
+                    type="password" 
+                    class="input" 
+                    name="password" 
+                    use:validators={[required, minLength(8), maxLength(64)]} 
+                />
+                <HintGroup for="password">
+                    <Hint for="password" on="required">This is a mandatory field</Hint>
+                    <Hint for="password" on="minLength" hideWhenRequired>Password must be at least 8 characters long</Hint>
+                    <Hint for="password" on="maxLength" hideWhenRequired>Password must be at most 64 characters long</Hint>
+                    <Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>
+                </HintGroup>
+                <div class="cut"></div>
+                <label for="password" class="placeholder">Password</label>
+            </div>
+            <div class="input-container ic2">
+                <input
+                    type="password"
+                    class="input" 
+                    name="passwordConfirmation"
+                    use:validators={[required, passwordMatch]}
+                />
+                <HintGroup for="password">
+                    <Hint for="password" on="required">This is a mandatory field</Hint>
+                    <Hint for="password" on="minLength" hideWhenRequired>Password must be at least 8 characters long</Hint>
+                    <Hint for="password" on="maxLength" hideWhenRequired>Password must be at most 64 characters long</Hint>
+                    <Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>
+                </HintGroup>
+                <div class="cut"></div>
+                <label for="password" class="placeholder">Confirm Password</label>
+            </div>
+            <button disabled={!$form2.valid} class="custom_button_gems">Register</button>
+        </div>
+    </form>
+</div>
 
 <style>
     :global(.touched:invalid) {
